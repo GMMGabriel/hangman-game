dd2812a9b07eb310b557bc54c5559e38f4a1a32a
Gabriel de Melo Marcondes <33481928+GMMGabriel@users.noreply.github.com>
2022-08-05 16:50:03 -0300

    removing way to change the theme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
 import { Link } from 'react-router-dom'
 
-import { useTheme } from '../hooks/useTheme'
-
 import '../styles/home.scss'
 
 export function Home() {
-  const { changeTheme } = useTheme()
 
   return (
     <div id="page-home">
@@ -16,15 +13,6 @@ export function Home() {
           <Link to="/singleplayer">Um jogador</Link>
           <Link to="/credits">Créditos</Link>
         </nav>
-
-        <div className="change-color-theme">
-          <p>mudar o tema da aplicação</p>
-          <ul className="theme-list">
-            <li><button className="button-theme-blue" onClick={() => changeTheme('theme-blue')}></button></li>
-            <li><button className="button-theme-green" onClick={() => changeTheme('theme-green')}></button></li>
-            <li><button className="button-theme-red" onClick={() => changeTheme('theme-red')}></button></li>
-          </ul>
-        </div>
       </main>
     </div>
   )
